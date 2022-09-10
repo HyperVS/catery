61bf6d770b5db4b2deec6e9b8eef655b896b3cbb
DrTheteSigma <halsailani@usf.edu>
2022-09-10 03:23:22 -0400

    Home page done

--- Frontend/src/components/Home.js
+++ Frontend/src/components/Home.js
@@ -4,18 +4,18 @@ const Home = () => {
   return (
     <div className='home-content'>
 
-      <div className='home-left'>
-        <h1>Connecting Clients to Professional event planners</h1>
+      <div className='home-description-div'>
+        <h1 className='home-description'>Connecting Clients to Professional event planners</h1>
         <div className='register'>
-                    <div>
-                      <h1>For Users</h1>
-                      <div>We are the leading software to connect you to the best even planners</div>
-                      <button>Start Planning!</button>
+                    <div className='Users-div'>
+                      <h2>For Users</h2>
+                      <h3>We are the leading software to connect you to the best even planners</h3>
+                      <button className='btn'>Start Planning!</button>
                     </div>
-                    <div>
-                      <h1>For Contracters</h1>
-                      <div>We are the leading software to connect you to customers</div>
-                      <button>Start taking Clients</button>
+                    <div className='Contractors-div'>
+                      <h2>For Contractors</h2>
+                      <h3>We are the leading software to connect you to customers</h3>
+                      <button className='btn'>Start taking Clients</button>
                     </div>
         </div>
       </div>
