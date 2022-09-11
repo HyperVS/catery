6ab3322aced3d750f401b452f03d9bcc6cc97eda
gulphater <dotanoobbich@mail.ru>
2022-09-11 08:23:47 +0300

    Commit before mergin with master

--- Frontend/src/routes/Account.js
+++ Frontend/src/routes/Account.js
@@ -1,11 +1,14 @@
 import "../styles/Account.css"
 import axios from "axios"; 
-
+import {signOut} from "firebase/auth";
 export default function Account() { 
     return(
         <>
         <p> Name: Name </p>
-        <button className="btn btn-logout"> Log Out </button>
+        <p> Email: Email </p>
+        <p> Role: Role </p>
+        <p> Rating: Rating </p>
+        <button className="btn btn-logout" onClick={() => signOut()}> Log Out </button>
         </>
     );
 }
