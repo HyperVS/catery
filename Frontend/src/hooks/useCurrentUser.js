b8361a43098781b475376c36a0cc63d89b983a74
Omar Abdelrahman <63892536+HyperVS@users.noreply.github.com>
2022-09-11 03:33:48 -0400

    Fix hook

--- Frontend/src/hooks/useCurrentUser.js
+++ Frontend/src/hooks/useCurrentUser.js
@@ -5,11 +5,14 @@ import axios from "axios";
 export const useCurrentUser = async () => {
     const [user, setUser] = useState();
     const firebaseUser = auth.currentUser();
-    try {
-        const result = await axios.get(`http://localhost:8080/v1/users/getById?id=${firebaseUser.uid}`)
-        setUser(result.data)
-    } catch (error) {
-        console.error(error);
-    }
+
+    useEffect(() => {
+        try {
+            const result = await axios.get(`http://localhost:8080/v1/users/getById?id=${firebaseUser.uid}`)
+            setUser(result.data)
+        } catch (error) {
+            console.error(error);
+        }
+    }, [])
     return { user };
 };
